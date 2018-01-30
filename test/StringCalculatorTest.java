244340c38d5be89d5e425604c658a58da5b2ae2c
Javaughn <javaughnmiller@gmail.com>
2018-01-30 18:41:27 -0500

    First commit

--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -1,19 +1,9 @@
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-//@RunWith(Arquillian.class)
-//public class StringCalculatorTest {
-//    @Deployment
-//    public static JavaArchive createDeployment() {
-//        return ShrinkWrap.create(JavaArchive.class)
-//                .addClass(StringCalculator.class)
-//                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-//    }
-//
-//}
+class StringCalculatorTest {
+
+
+
+}
