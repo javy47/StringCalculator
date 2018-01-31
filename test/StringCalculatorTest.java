50f60cd18840acdfd6d0215ee87d8977f834e949
Javaughn <javaughnmiller@gmail.com>
2018-01-30 19:43:44 -0500

    4th commit

--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -6,12 +6,13 @@ class StringCalculatorTest {
 
     @Test
     void AddTest(){
-
+        //Empty String
         String testEmpty = " ";
         StringCalculator test = new StringCalculator();
         assertEquals(0,test.Add(testEmpty),"this should return 0");
-
+        //One Item
         String testOneItem = "0";
+        assertEquals(31,test.Add(testOneItem),"this should return 31");
 
         String testTwoItem = "1,2";
 
