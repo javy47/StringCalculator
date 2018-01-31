f2b09026b87b59d8eb0ed2bd623c85692b3501c8
Javaughn <javaughnmiller@gmail.com>
2018-01-30 19:36:43 -0500

    3rd Commit

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,13 +1,13 @@
 public class StringCalculator {
 
 
-    int Add( String numbers){
+    int Add(String numbers) {
+        int sum = 25;
+        if (numbers == " ") {
+            return 0;
+        }
 
-
-
-        return 0;
+        return sum;
     }
-    public static void main(String[] args) {
+}
 
-    }
-    }
