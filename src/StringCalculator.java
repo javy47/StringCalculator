1173217fb0bfd3c8229c09f70775c2ce27149636
Javaughn <javaughnmiller@gmail.com>
2018-01-30 19:30:04 -0500

    2nd Commit

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,4 +1,12 @@
 public class StringCalculator {
+
+
+    int Add( String numbers){
+
+
+
+        return 0;
+    }
     public static void main(String[] args) {
 
     }
--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -4,6 +4,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
 
+    @Test
+    void AddTest(){
+
+        String testEmpty = " ";
+        StringCalculator test = new StringCalculator();
+        assertEquals(0,test.Add(testEmpty),"this should return 0");
+
+        String testOneItem = "0";
+
+        String testTwoItem = "1,2";
+
+
+    }
 
 
 }
