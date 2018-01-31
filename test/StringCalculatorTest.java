98a0ea8481dabbffcc3a4def0303e517c342797b
Javaughn <javaughnmiller@gmail.com>
2018-01-30 20:08:30 -0500

    6th commit

--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -11,12 +11,11 @@ class StringCalculatorTest {
         StringCalculator test = new StringCalculator();
         assertEquals(0,test.Add(testEmpty),"this should return 0");
         //One Item
-
         String testOneItem = "31";
         assertEquals(31,test.Add(testOneItem),"this should return 31");
-
+        //Two Items
         String testTwoItem = "1,2";
-
+        assertEquals(3,test.Add(testTwoItem),"this should return 3");
 
     }
 
